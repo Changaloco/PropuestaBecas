31ed8d8824a033290879dfa3ab01b2f6b129aef6
changaloco <e.alamilla318@gmail.com>
2020-08-27 19:33:09 -0500

    xd last

--- src/dist/index.dev.js
+++ src/dist/index.dev.js
@@ -2,11 +2,14 @@
 
 var express = require('express');
 
+var morgan = require('morgan');
+
 var app = express(); //settings
 
 app.set('port', process.env.PORT || 3000); //Routes
 //Middlewares 
-//Static files
+
+app.use(morgan('dev')); //Static files
 //STarting the server
 
 app.listen(app.get('port'), function () {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const morgan = require('morgan')
 const app = express();
 
 //settings
 app.set('port',process.env.PORT|| 3000);
 //Routes
 //Middlewares 
+app.use(morgan('dev'));
 //Static files
 //STarting the server
 app.listen(app.get('port'),()=>{
